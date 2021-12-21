bf4dfbb2b3b874ad2855d158a9cfab06abfc918d
yourway <95677792+yourWayL@users.noreply.github.com>
2021-12-21 15:28:54 +0800

    V1.0.2

--- app/Client/Config/database.php
+++ app/Client/Config/database.php
@@ -9,7 +9,7 @@ return [
             'host'        => '127.0.0.1',
             'port'        => 3306,//MySQL端口 默认3306 可选参数
             'user'        => 'root',
-            'password'    => 'secxun@2019X',
+            'password'    => 'dasdasdas@2019X',
             'database'    => 'dfxc',
             'timeout'     => 120, // 仅影响connect超时时间，不影响query和execute方法,参考`客户端超时规则`
             'charset'     => 'utf8mb4',
