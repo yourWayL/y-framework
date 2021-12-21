d1b33baa13d3e0605552974c2dedddbbb72a696e
yourway <95677792+yourWayL@users.noreply.github.com>
2021-12-21 15:28:04 +0800

    1.0.2

--- app/Client/Config/email.php
+++ app/Client/Config/email.php
@@ -2,8 +2,8 @@
 return [
     //默认配置
     'default' => [
-        'UserName' => 'admin@secxun.com',
-        'Password' => 'Ax123..',
-        'FromName'=>'安巽科技有限公司'
+        'UserName' => 'admin@321312.com',
+        'Password' => '321312dasdas..',
+        'FromName'=>'dasdasdasdas'
     ],
 ];
